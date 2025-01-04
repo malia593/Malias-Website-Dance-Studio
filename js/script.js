bb69e18ddc2d67ba879fc8ebe035931b1a1d6ecf
Malia Paula Alibes Bagur <u1995549@campus.eram.cat>
2025-01-04 15:15:32 +0100

    principle page

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');}
+
+
+
+var columns = document.querySelectorAll(".col");
+
+
